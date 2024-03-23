a4a81ca48408847eee988566a2bcc61440141b30
Richard Marshall <rgm@linux.com>
2024-03-23 10:37:13 -0700

    Label builtin functions / variables

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -16,7 +16,7 @@
  "error"
  "esi"
  "fallthrough"
- ;"goto"
+ "goto"
  "if"
  "include"
  "local"
@@ -85,15 +85,26 @@
 
 (call_statement
   ident: (ident) @function.call)
+[
+ (call_statement
+  ident: (ident) @_content (#match? @_content "^(accept|bin|digest|time|std|header|querystring|math|addr|fastly|setcookie|ratelimit|json|utf8|h2|h3|table|uuid|testing|assert)\.\b"))
+] @function.builtin
+
 
 ; Identifiers
 
 (type) @type
 (ident) @variable
 (dot_ident) @variable
+[
+ ((dot_ident) @_content (#match? @_content "^(req|bereq|resp|beresp|obj|fastly_info|backend|stale|client|quic|tls|transport|time|now|esi|math\.[A-Z0-9_]+|director|fastly|re|workspace|ratecounter|segmented_caching|server|waf)\b"))
+ ((ident) @_content (#match? @_content "^(now)\b"))
+] @variable.builtin
+(statement_label) @label
 
 ; Literals
 (number) @number
 (string) @string @spell
-(escape_sequence) @escape
+(escape_sequence) @string.escape
 (bool) @constant.builtin
+(states) @constant.builtin
